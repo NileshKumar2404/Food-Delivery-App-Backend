5dd4e503edf4ecf5bd94b213f6f9976c09650ef1
Nilesh Kumar <nileshkr2404@gmail.com>
2025-08-17 01:20:01 +0530

    Initialize Express app and MongoDB connection setup

--- src/db/index.js
+++ src/db/index.js
@@ -4,8 +4,7 @@ import {DB_NAME} from "../constants.js"
 const connectDB = async() => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`Mongodb Connection successfully: ${connectionInstance.connection.host}`);
-        
+        console.log(`\nConnected to MongoDB: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error(`Mongodb connection error: `, error);
         process.exit(1)
